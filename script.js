fe0c976ffa2b71c061e09ce9c934614d8537e00c
Tobel <tobelwubshet@gmail.com>
2018-03-04 03:07:25 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function changeGreen(){
+            document.getElementById("firstPanel").style.background="#01bcd6";
+        
+            document.getElementById("tab-content").style.background="#01bcd6";
+        }
+function changeRed(){
+            document.getElementById("secondPanel").style.background=" #E73C7E";
+           
+            document.getElementById("tab-content").style.background="#E73C7E";
+        }
+function changeBlue(){
+            document.getElementById("thirdPanel").style.background="#ff3030";
+          
+            document.getElementById("tab-content").style.background="#ff3030";
+        }
